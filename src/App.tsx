900e53029acc60532ac72dd79d27c375677ab10d
yuutaDragon <guilherme.rodrigues@estudante.ufscar.br>
2023-01-26 18:05:04 -0300

    responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import { AboutMe } from "./components/AboutMe";
-import { Card } from "./components/Card";
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import { Home } from "./components/Home";
 import { Projects } from "./components/Projects";
 
-import "./styles/App.css";
-
 function App() {
   return (
     <>
-     
       <Header />
       <Home />
       <AboutMe />
